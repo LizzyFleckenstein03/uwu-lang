8b1de3b34222ec6d8e6c99f7897b9cbd5fbba264
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-12-30 18:34:12 +0100

    Update usage help

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
-		error("usage: %s <module>\n", argv[0]);
+		error("usage: %s <module> <args>\n", argv[0]);
 
 	run_module(argv[0], argv[1], argc > 2 ? (size_t) argc - 2 : 0, &argv[2]);
 	return 0;
